ee2f5c66bb60fe47cd4ecc1de0a946effe2c5a9a
Ashish Kapoor <AshishKapoor@users.noreply.github.com>
2021-07-15 14:03:36 +0530

    Added basic routing and config

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+  <title>Pokedex | About</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p>I am supposed to write cool description of this pokedex project here. (later maybe)</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+  import {pokemon} from '../stores/pokemart'
+	import Card from '../components/card.svelte'
+</script> 
+
+<svelte:head>
+  <title>Pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">Pokedex</h1>
+{#each $pokemon as pokeman}
+  <Card {pokeman}></Card>
+{/each}
