78c967c8735886aa4db1ac76fbb24365bb18967a
Ashish Kapoor <swiftobjc@gmail.com>
2021-08-16 18:56:21 +0530

    deploy updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,10 @@
 // svelte.config.cjs
-import adapter from '@sveltejs/adapter-static';
+import static_adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 
 export default {
 	kit: {
 		target: '#svelte',
-		adapter: adapter({
-			pages: 'build/_app/pages',
-			assets: 'build/_app/assets',
-			fallback: '200.html'
-		})
+		adapter: static_adapter(),
 	}
 };
