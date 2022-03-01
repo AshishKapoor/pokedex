09cf806716b660aeddf36d56afaf97680873085a
Ashish Kapoor <swiftobjc@gmail.com>
2022-03-02 01:33:19 +0530

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,4 +3,4 @@
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8 uppercase">About</h1>
-<p>I am supposed to write cool description of this pokedex project here. (later maybe)</p>
+<p style="text-align: center;">Hi! I am Ashish Kapoor and this is a fun project to dabble with SvelteKit.</p>
